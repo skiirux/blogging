9453da47092a40d96db7abbea899231ae8aa167e
skiirux <mohitkumar637@gmail.com>
2021-06-27 20:45:40 +0530

    added the postlist file

--- js/bloglist.js
+++ js/bloglist.js
@@ -2,11 +2,11 @@
 var trashFunctions = function (numberOfPosts) {
 
     for (var i = 0; i < numberOfPosts; i++) {
-        // Get the trash modal
-        var modal = document.getElementsByClassName('trash-modal')[i];
-
         // Get the div that opens the modal
         var div = document.getElementsByClassName("post-trash")[i];
+
+        // Get the trash modal
+        var modal = document.getElementsByClassName('trash-modal')[i];
 
         // Get the <span> element that closes the modal
         var close = document.getElementsByClassName("trash-modal-button-no")[i];
@@ -26,5 +26,5 @@ var trashFunctions = function (numberOfPosts) {
 trashFunctions(5)
 
 function navigateToPost() {
-    location.href = "post.html";
+    location.href = "postslist.html";
 }
